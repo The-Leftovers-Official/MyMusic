c6a24a5ed7d029fe71daf683859d0dae2cd602d7
CIANDT\ipannunzio <ipannunzio@ciandt.com>
2022-07-27 15:19:18 -0300

    [WIP] Implementing Token validation

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/AccessController.java
@@ -0,0 +1,9 @@
+package com.ciandt.summit.bootcamp2022.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("acessos")
+@RestController
+public class AccessController {
+}
