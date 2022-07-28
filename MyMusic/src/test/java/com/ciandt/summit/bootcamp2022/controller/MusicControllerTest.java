2505998759368de01051fc53359bb322152b91b4
ContagiousMisfit <isacaovila@gmail.com>
2022-07-28 10:30:33 -0300

    [WIP] Controller Test Class drafted

---
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controller/MusicControllerTest.java
@@ -0,0 +1,16 @@
+package com.ciandt.summit.bootcamp2022.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MusicControllerTest {
+
+    //TODO: Buscar + de 2 caracteres deve retornar Artista E Música
+
+    // TODO: Deve Retornar 204 caso não encontre
+
+    // TODO: Deve Retornar 400 caso envie apenas 1 caracter
+
+    // TODO: Busca deve vir ordenada pelo nome do artista e depois pelo nome da música
+
+}
