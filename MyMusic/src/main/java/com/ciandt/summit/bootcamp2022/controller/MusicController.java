67b62175cc80e5c9f2db08a71c39779e84fbf79f
Dara S. Mariano <darasouza@ciandt.com>
2022-07-29 06:40:38 -0300

    [WIP] - Fixing interceptor error

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -1,18 +1,19 @@
 package com.ciandt.summit.bootcamp2022.controller;
 
+import com.ciandt.summit.bootcamp2022.exceptions.AuthorizedHandler;
 import com.ciandt.summit.bootcamp2022.http.TokenClient;
+import com.ciandt.summit.bootcamp2022.http.dto.CreateAuthorizerRequestData;
 import com.ciandt.summit.bootcamp2022.http.dto.CreateTokenRequestDataDto;
+import com.ciandt.summit.bootcamp2022.http.dto.TokenAuthorizerRequestDto;
 import com.ciandt.summit.bootcamp2022.http.dto.TokenRequestDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Slf4j
 @RestController
 @RequestMapping("/api/v1/music")
 public class MusicController {
@@ -20,13 +21,32 @@ public class MusicController {
     private final TokenClient tokenClient;
 
     @GetMapping
-    public ResponseEntity<String> get() {
-        TokenRequestDto token = TokenRequestDto.builder()
-                .data(CreateTokenRequestDataDto.builder().name("Fulano").build())
+    public ResponseEntity<String> get(@RequestHeader("Username") String username) {
+
+        if (username == "")
+            throw new AuthorizedHandler.InvalidRequestHeaderException();
+
+        Boolean authorized = isAuthorized(username);
+
+        return ResponseEntity.ok().build();
+    }
+
+    private Boolean isAuthorized(String username) {
+
+        TokenRequestDto userName = TokenRequestDto.builder()
+                .data(CreateTokenRequestDataDto.builder().name(username).build())
                 .build();
+        String token = tokenClient.getToken(userName).getBody();
 
+        TokenAuthorizerRequestDto authorization = TokenAuthorizerRequestDto.builder()
+                .data(CreateAuthorizerRequestData.builder().name(username).token(token).build())
+                        .build();
 
+        String returnedAuthorization = tokenClient.getAuthorization(authorization).getBody();
 
-        return tokenClient.getToken(token);
+        if (!returnedAuthorization.equals("ok"))
+            throw new IllegalArgumentException("Not authorized!");
+
+        return true;
     }
 }
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/exceptions/AuthorizedHandler.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/exceptions/AuthorizedHandler.java
@@ -2,13 +2,19 @@ package com.ciandt.summit.bootcamp2022.exceptions;
 
 
 import com.ciandt.summit.bootcamp2022.exceptions.dto.ValidationErrorDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class AuthorizedHandler {
+public class AuthorizedHandler extends ResponseEntityExceptionHandler {
 
 
     @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
@@ -16,5 +22,35 @@ public class AuthorizedHandler {
     private ValidationErrorDto handle(IllegalArgumentException exception){
         return ValidationErrorDto.builder().message(exception.getMessage()).build();
     }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public static class InvalidRequestHeaderException extends RuntimeException {
+
+        public InvalidRequestHeaderException() {
+            super("Required request header 'Username' for method parameter type String is not present");
+        }
+    }
+
+
+    @ExceptionHandler(value = {
+            MissingRequestHeaderException.class,
+            InvalidRequestHeaderException.class
+    })
+    protected ResponseEntity<Object> handleRequestHeaderException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.builder()
+                .status(String.valueOf(HttpStatus.UNAUTHORIZED.value()))
+                .reason(ex.getMessage()).build());
+    }
+
+
+    @AllArgsConstructor
+    @Getter
+    @Builder
+    public static class ErrorResponse {
+
+        private String status;
+        private String reason;
+    }
+
 
 }
