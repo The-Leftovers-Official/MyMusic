1a659c7127e8b3e194e89c6707c782373113902a
Larissa Labanca <ladyweigann@gmail.com>
2022-08-04 17:23:36 -0300

    [DONE] - Descriptions included on swagger

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/adapter/controller/CacheController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/adapter/controller/CacheController.java
@@ -1,5 +1,6 @@
 package com.ciandt.summit.bootcamp2022.adapter.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/cache")
 public class CacheController {
 
+    @Operation(summary = "url to clear cache")
     @GetMapping("/aW52YWxpZGEgcmVsYXTDs3JpbyBkZSB2ZW5kYXM")
     @CacheEvict(value = "listOfMusicByNameOrArtist", allEntries = true)
     public ResponseEntity<?> invalidateMusicCache() {
