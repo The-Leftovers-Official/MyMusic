ce80183c447c2c44813913555188f9e82d60cc67
Larissa Labanca <ladyweigann@gmail.com>
2022-08-04 17:23:36 -0300

    [DONE] - Descriptions included on swagger

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/adapter/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/adapter/controller/MusicController.java
@@ -2,9 +2,16 @@ package com.ciandt.summit.bootcamp2022.adapter.controller;
 
 import com.ciandt.summit.bootcamp2022.adapter.controller.dto.MusicDto;
 import com.ciandt.summit.bootcamp2022.adapter.controller.dto.ResponseWrapper;
+import com.ciandt.summit.bootcamp2022.entity.Music;
 import com.ciandt.summit.bootcamp2022.http.TokenAuthorizedClientUtils;
 import com.ciandt.summit.bootcamp2022.infra.entity.MusicEntity;
 import com.ciandt.summit.bootcamp2022.usecase.MusicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
@@ -29,10 +36,20 @@ public class MusicController {
     @Autowired
     private TokenAuthorizedClientUtils tokenAuthorizedClient;
 
-
+    @Operation(summary = "Get music by name or artist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the music",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Music.class)) }),
+            @ApiResponse(responseCode = "204", description = "No music found with these parameters",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Try to search with less than 2 letters parameter",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Not authorized",
+                    content = @Content)})
     @GetMapping
     @Cacheable(value = "listOfMusicByNameOrArtist")
-    public ResponseEntity<ResponseWrapper> getMusicByNameOrArtist(@RequestParam(required = false) String filtro,
+    public ResponseEntity<ResponseWrapper> getMusicByNameOrArtist(@Parameter(description = "filter can be name of the music or artist") @RequestParam(required = false) String filtro,
                                                                   @PageableDefault(page = 0) Pageable pageable,
                                                                   @RequestHeader("Username") String username) {
 
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/adapter/controller/PlaylistController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/adapter/controller/PlaylistController.java
@@ -2,9 +2,16 @@ package com.ciandt.summit.bootcamp2022.adapter.controller;
 
 import com.ciandt.summit.bootcamp2022.adapter.controller.dto.MusicInformationDto;
 import com.ciandt.summit.bootcamp2022.adapter.controller.dto.ResponseWrapper;
+import com.ciandt.summit.bootcamp2022.entity.Music;
 import com.ciandt.summit.bootcamp2022.entity.PlaylistRepository;
 import com.ciandt.summit.bootcamp2022.exceptions.AuthorizedHandler;
 import com.ciandt.summit.bootcamp2022.http.TokenAuthorizedClientUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +28,20 @@ public class PlaylistController {
     @Autowired
     private PlaylistRepository playlistRepository;
 
+    @Operation(summary = "Add music into playlist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Music added successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Music.class)) }),
+            @ApiResponse(responseCode = "400", description = "Music or playlist don't exists on database",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Not authorized",
+                    content = @Content)})
     @PostMapping("{playlistId}/musics")
     @Transactional
-    public ResponseEntity<ResponseWrapper> addMusicToPlaylist(@PathVariable String playlistId,
+    public ResponseEntity<ResponseWrapper> addMusicToPlaylist(@Parameter(description = "Id of the playlist where you want to add a music") @PathVariable String playlistId,
                                                               @RequestBody MusicInformationDto musicInformationDto,
-                                                              @RequestHeader("Username") String username) {
+                                                              @Parameter(description = "Username to get authorization") @RequestHeader("Username") String username) {
 
         if (username.isEmpty())
             throw new AuthorizedHandler.InvalidRequestHeaderException();
